7182a8094da278100962be8dccdf19ca47217225
Mesharialmadi1 <Mesharialmadi1@gmail.com>
2023-01-24 12:10:51 +0300

    add comment

--- src/main/java/com/example/School/controller/StudentController.java
+++ src/main/java/com/example/School/controller/StudentController.java
@@ -41,7 +41,7 @@ public class StudentController {
     }
 
     //only change path when doing something else
-    @PostMapping("/addStudents")
+    @PostMapping("/addStudents")//yyy
     public ResponseEntity<?> newStudent(@RequestBody List<Student> students) { //object array?
         return new ResponseEntity<>(studentService.addStudents(students),HttpStatus.CREATED); //201
     }
