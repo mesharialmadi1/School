ef736827d1deffdf92706965918338c3ed6442a6
Mesharialmadi1 <Mesharialmadi1@gmail.com>
2023-01-24 16:16:25 +0300

    testmerge

--- src/main/java/com/example/School/Mappers/StudentMapper.java
+++ src/main/java/com/example/School/Mappers/StudentMapper.java
@@ -2,7 +2,7 @@ package com.example.School.Mappers;
 import com.example.School.Mapstruct.StudentDto;
 import com.example.School.model.Student;
 import org.mapstruct.Mapper;
-import java.util.List;
+//import java.util.List;
 @Mapper
 public interface StudentMapper {
     StudentDto studentToStudentDto(Student student);
