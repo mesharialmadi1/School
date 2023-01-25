97330187099d76562ef53f48583897fa9cae72f0
mesharialmadi1 <86104507+mesharialmadi1@users.noreply.github.com>
2023-01-25 08:48:42 +0300

    Update SchoolApplication.java

--- src/main/java/com/example/School/SchoolApplication.java
+++ src/main/java/com/example/School/SchoolApplication.java
@@ -1,7 +1,7 @@
 package com.example.School;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; //git --rebase test
 
 @SpringBootApplication
 public class SchoolApplication {
